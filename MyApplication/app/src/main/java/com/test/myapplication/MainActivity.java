3250def13a1b9e253814533f0332363e832a0f77
Anshul Agrawal <anshulagrawal.1989@gmail.com>
2021-08-12 23:04:13 +0530

    added a very basic work manager sample

---
+++ MyApplication/app/src/main/java/com/test/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.test.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
+
+import java.time.Duration;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    OneTimeWorkRequest.Builder builder = new OneTimeWorkRequest.Builder(DelayedPrintWorker.class);
+    OneTimeWorkRequest uploadWorkRequest = null;
+    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+      uploadWorkRequest = builder.setInitialDelay(Duration.ofSeconds(10)).build();
+    } else {
+      uploadWorkRequest = builder.build();
+    }
+    WorkManager.getInstance(this).enqueue(uploadWorkRequest);
+  }
+}
